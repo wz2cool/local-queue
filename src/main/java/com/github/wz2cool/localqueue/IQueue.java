7ac5be73560deda476fb350d99bec29851069297
wz2cool <wz2cool@live.cn>
2025-01-26 20:39:07 +0800

    remove get reader using config

--- src/main/java/com/github/wz2cool/localqueue/IQueue.java
+++ src/main/java/com/github/wz2cool/localqueue/IQueue.java
@@ -1,11 +1,8 @@
 package com.github.wz2cool.localqueue;
 
-import com.github.wz2cool.localqueue.model.config.SimpleReaderConfig;
-
 public interface IQueue {
     boolean offer(String message);
 
     IReader getReader(String readerKey);
-
-    IReader getReader(SimpleReaderConfig config);
+    
 }
